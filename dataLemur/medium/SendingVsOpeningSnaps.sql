6f19cc858b65a5edd902ccea8a159963881cfa7e
Newns <NEWNSS@CHOP.EDU>
2023-09-01 10:52:18 -0400

    save an unsaved file

--- dataLemur/medium/SendingVsOpeningSnaps.sql
+++ dataLemur/medium/SendingVsOpeningSnaps.sql
@@ -94,3 +94,37 @@ INNER JOIN age_breakdown
   ON activities.user_id = age_breakdown.user_id 
 WHERE activities.activity_type IN ('send', 'open') 
 GROUP BY age_breakdown.age_bucket;
+
+
+-- Given solution 2: CTE
+WITH snaps_statistics AS (
+  SELECT 
+    age_breakdown.age_bucket, 
+    SUM(
+      CASE
+        WHEN activities.activity_type = 'send' 
+        THEN activities.time_spent 
+        ELSE 0
+      END
+    ) AS send_timespent, 
+    SUM(
+      CASE
+        WHEN activities.activity_type = 'open' 
+        THEN activities.time_spent
+        ELSE 0 
+      END
+    ) AS open_timespent,
+    SUM(activities.time_spent) AS total_timespent 
+FROM activities
+INNER JOIN age_breakdown ON 
+  activities.user_id = age.user_id 
+WHERE activities.activity_type IN ('send', 'open') 
+GROUP BY age_breakdown.age_bucket) 
+
+
+SELECT 
+  age_bucket, 
+  ROUND(100.0 * send_timespent / total_timespent, 2) AS send_perc, 
+  ROUND(100.0 * open_timespent / total_timespent, 2) AS open_perc 
+FROM snaps_statistics
+;
