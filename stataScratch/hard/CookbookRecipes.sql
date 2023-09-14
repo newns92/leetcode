df6d941b185aa4452c2b5f221eeff63e108b0fb8
Newns <s.newns92@gmail.com>
2023-09-14 13:06:15 -0400

    finish statascratch free hard SQL questions

---
+++ stataScratch/hard/CookbookRecipes.sql
@@ -0,0 +1,140 @@
+/*
+You are given the table with titles of recipes from a cookbook and their page numbers. 
+
+You are asked to represent how the recipes will be distributed in the book.
+
+Produce a table consisting of three columns: left_page_number, left_title and right_title. 
+
+The k-th row (counting from 0), should contain the number and the title of the page with the number 2 * k in the first and second columns respectively, 
+    and the title of the page with the number (2 * k) + 1 in the third column.
+
+Each page contains at most 1 recipe. 
+If the page does not contain a recipe, the appropriate cell should remain empty (NULL value). 
+Page 0 (the internal side of the front cover) is guaranteed to be empty.
+
+cookbook_titles
+page_number:    int
+title:          varchar
+*/
+
+
+-- ATTEMPT: GENERATE_SERIES, 2 CTE's, and a Self-JOIN to squash/merge two columns
+With Series AS (
+    -- HAS to be DISTINCT because it duplicates the series in the column for some reason?
+    SELECT DISTINCT
+        -- COUNT(*)
+        -- Need to generate a series of page numbers from 0 to the maximum page number in the cookbook
+        -- Then, do steps of 2 to get only the left page numbers
+        -- https://www.postgresql.org/docs/current/functions-srf.html
+        --  - GENERATE_SERIES(start int/bigint, end int/bigint [, step int/bigint])
+        GENERATE_SERIES((SELECT MIN(page_number)::INT FROM cookbook_titles),
+                        (SELECT MAX(page_number)::INT FROM cookbook_titles),
+                        2) - 1
+            AS left_page_number
+    FROM cookbook_titles
+    -- LIMIT 5
+    -- ORDER BY left_page_number
+),
+
+Test AS (
+    -- This query will be squashing and merging the below left_title and right_title fields
+    --  when future_left_page_number is equal to left_page_number
+    SELECT
+        -- This is mainly to get the correct right page titles lined up with what will
+        --  be the left page number
+        page_number - 1 AS future_left_page_number,
+        -- for the future JOIN
+        left_page_number,
+        -- If it's an even page, then we have the left title
+        CASE
+            WHEN page_number % 2 = 0
+            THEN title
+            ELSE NULL
+        END AS left_title,
+        -- If it's an odd page, then we have the right title
+        CASE
+            WHEN page_number % 2 <> 0 -- OR page_number - 1 = 0
+            THEN title
+            ELSE
+                -- Edge case of page 0
+                CASE
+                    WHEN page_number - 1 = 0
+                    THEN title
+                    ELSE NULL
+                END
+        END AS right_title
+    FROM cookbook_titles
+    -- Need ALL left_page_numbers from the series to use in final query
+    FULL OUTER JOIN Series ON
+        cookbook_titles.page_number = Series.left_page_number
+    -- ORDER BY left_page_number
+)
+
+-- SELECT * FROM Test
+
+SELECT
+    -- t1.future_left_page_number,
+    t1.left_page_number,
+    t1.left_title,
+    t2.right_title
+    -- *
+FROM Test t1
+-- Keep ALL left page numbers by making sure the left_page_number field is
+--  on the correct side of the JOIN
+--      - Could do a RIGHT JOIN with t1.future_left_page_number = t2.left_page_number
+--          WHERE t2.left_page_number IS NOT NULL
+--      - Would also have to swap "t1" and "t2" in the SELECT statement
+LEFT JOIN Test t2 ON
+    t1.left_page_number = t2.future_left_page_number
+WHERE t1.left_page_number IS NOT NULL
+;
+
+
+-- SOLUTION: Faster than mine, uses GENERATE_SERIES, 2 CTE's, and ROW_NUMBER + STRING_AGG in final query
+--  - Not sure why STRING_AGG is needed?
+-- CTE 1
+WITH series AS (
+    -- Need to generate a series of page numbers from 0 to the maximum page number in the cookbook
+    -- https://www.postgresql.org/docs/current/functions-srf.html
+    --  - GENERATE_SERIES(start int/bigint, end int/bigint [, step int/bigint])    
+    SELECT
+        generate_series AS page_number
+    FROM generate_series(0, (SELECT max(page_number) FROM cookbook_titles))
+),
+
+-- CTE 2
+cookbook_titles_v2 AS (
+    -- Match up/JOIN the page numbers from the given table with the generated series from above
+    SELECT
+        series.page_number,
+        cookbook_titles.title
+    FROM series
+    LEFT JOIN cookbook_titles ON
+        series.page_number = cookbook_titles.page_number
+)
+
+-- SELECT * FROM cookbook_titles_v2
+
+SELECT
+    -- Calculate the left_page_number
+    (ROW_NUMBER() OVER(ORDER BY page_number / 2) - 1) * 2 AS left_page_number,
+    -- STRING_AGG concatenates a list of strings and places a separator between them
+    -- https://www.postgresqltutorial.com/postgresql-aggregate-functions/postgresql-string_agg-function/
+    -- STRING_AGG(expression, separator [, order_by_clause])
+    -- MUST use STRING_AGG (an aggregate function) instead of needing page_number in a GROUP BY clause
+    STRING_AGG(
+        CASE
+            WHEN page_number % 2 = 0
+            THEN title
+        END
+    , ',') AS left_title,
+    STRING_AGG(
+        CASE
+            WHEN page_number % 2 = 1
+            THEN title
+        END
+    , ',') AS right_title
+FROM cookbook_titles_v2
+-- Group the rows in pairs, representing the double-page spreads in the cookbook
+GROUP BY page_number / 2
+;
---
+++ stataScratch/hard/RetentionRate.sql
@@ -0,0 +1,139 @@
+/*
+Find the monthly retention rate of users for each account separately for Dec 2020 and Jan 2021. 
+
+Retention rate is the percentage of active users an account retains over a given period of time. 
+
+In this case, assume the user is retained if he/she stays with the app in any future months. 
+
+For example, if a user was active in Dec 2020 and has activity in any future month, consider them retained for Dec. 
+
+You can assume all accounts are present in Dec 2020 and Jan 2021. 
+
+Your output should have the account ID and the Jan 2021 retention rate divided by Dec 2020 retention rate.
+*/
+
+
+-- ATTEMPT: Convoluted with 3 complicated CTE's
+
+-- After inspecting data, accounts A1 and A2 have data from Dec-Feb, and
+--  account A3 only has data from Dec-Jan
+
+-- SELECT DISTINCT
+--     *
+--     -- account_id,
+--     -- -- date
+--     -- EXTRACT(MONTH FROM date) AS event_month
+-- FROM sf_events
+-- ORDER BY account_id, -- event_month
+--     date,
+--     user_id
+-- LIMIT 20
+-- ;
+
+WITH Test AS (
+    SELECT
+        -- *,
+        account_id,
+        user_id,
+        -- NOTE: EXTRACT() returns a DOUBLE PRECISION value
+        EXTRACT(MONTH FROM date) AS current_month,
+        EXTRACT(MONTH FROM DATE_TRUNC('MONTH', date + INTERVAL '1 month')) AS next_month,
+        -- Need the following field for filtering later on
+        -- https://database.guide/get-the-first-day-of-the-month-in-postgresql/
+        DATE_TRUNC('MONTH', date)::DATE AS current_month_start
+    FROM sf_events
+    -- WHERE date < '2021-02-01'
+    GROUP BY
+        -- Make sure each row is unique
+        EXTRACT(MONTH FROM date),
+        EXTRACT(MONTH FROM DATE_TRUNC('MONTH', date + INTERVAL '1 month')),
+        DATE_TRUNC('MONTH', date)::DATE,
+        account_id,
+        user_id
+    ORDER BY account_id, EXTRACT(MONTH FROM date), user_id
+    -- LIMIT 10
+),
+
+-- SELECT * FROM Test
+
+-- SELECT DISTINCT t2.account_id, t2.user_id, t2.current_month 
+-- FROM Test t2 
+-- LEFT JOIN Test t1 ON t1.account_id = t2.account_id AND t1.user_id = t2.user_id
+-- ORDER BY t2.account_id, t2.user_id, t2.current_month
+
+Retained AS (
+    SELECT
+        account_id,
+        user_id,
+        current_month,
+        -- next_month,
+        -- current_month_start,
+        -- Below will find out if the account_id + user_id + month combo (for the next month) is present in
+        --  the table, meaning the user in the account has been retained for the next month
+        CASE
+            WHEN (account_id, user_id, next_month) IN (
+                SELECT DISTINCT 
+                    t2.account_id,
+                    t2.user_id,
+                    t2.current_month
+                FROM Test t2 
+                LEFT JOIN Test t1 ON 
+                    t1.account_id = t2.account_id AND t1.user_id = t2.user_id)
+            THEN 'retained'
+            ELSE 'lost'
+        END AS retained_flag
+    FROM Test
+    -- Only need December and January retention rates
+    WHERE current_month_start < '2021-02-01'
+    -- ORDER BY account_id, next_month, user_id
+),
+
+-- SELECT * FROM Retained
+
+RetentionRates AS (
+    SELECT
+        -- *
+        account_id,
+        -- current_month,
+        CASE
+            WHEN current_month = 1
+            THEN
+                SUM(
+                    CASE
+                        WHEN retained_flag = 'retained'
+                        THEN 1
+                        ELSE 0
+                    END
+                ) -- AS retained_users
+                /
+                COUNT(DISTINCT user_id)::FLOAT -- AS total_users,
+        END AS jan_retention_rate,
+        CASE
+            WHEN current_month = 12
+            THEN
+                SUM(
+                    CASE
+                        WHEN retained_flag = 'retained'
+                        THEN 1
+                        ELSE 0
+                    END
+                ) -- AS retained_users
+                /
+                COUNT(DISTINCT user_id)::FLOAT -- AS total_users,
+        END AS dec_retention_rate    
+    FROM Retained
+    GROUP BY account_id, current_month
+)
+
+SELECT
+    t1.account_id,
+    t1.jan_retention_rate
+        /
+        t2.dec_retention_rate
+    AS retention_rate
+FROM RetentionRates t1
+INNER JOIN RetentionRates t2
+    ON t1.account_id = t2.account_id
+WHERE t1.jan_retention_rate IS NOT NULL AND
+    t2.dec_retention_rate IS NOT NULL
+;
